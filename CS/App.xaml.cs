81b16f85fad145bee017ed3eb4a23b4105233fc0
MariyaGordeeva <mariya.gordeeva@devexpress.com>
2025-01-16 18:34:47 +0400

    net 9.0

--- CS/App.xaml.cs
+++ CS/App.xaml.cs
@@ -3,7 +3,9 @@
 public partial class App : Application {
     public App() {
         InitializeComponent();
+    }
 
-        MainPage = new AppShell();
+    protected override Window CreateWindow(IActivationState activationState) {
+        return new Window(new AppShell());
     }
 }
